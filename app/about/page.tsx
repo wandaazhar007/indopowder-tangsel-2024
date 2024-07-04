2e158313f0e298a6135380fa48b0ce4a5028afff
wanda azhar <wandaazhar@gmail.com>
2024-07-04 00:41:22 -0400

    revision

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './about.scss';
-import { faGlassCheers, faHospitalAlt, faMoneyBill, faQuoteLeftAlt, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGlassCheers, faHandshake, faHospitalAlt, faMoneyBill, faQuoteLeftAlt, faUser, faUserFriends } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image';
 const AboutPage = () => {
 
@@ -10,37 +10,33 @@ const AboutPage = () => {
         <div className="row1">
           <div className="gridRow1">
             <div className="box box1">
-              <FontAwesomeIcon icon={faUser} className="icon" />
+              <FontAwesomeIcon icon={faHandshake} className="icon" />
               <span>
-                Ini saatnya Anda menjadi pemilik Waralaba / Franchisor Nya
+                Ambil langkah besar dalam bisnis minuman Anda dengan menjadi bagian dari jaringan waralaba kami yang sukses!
               </span>
             </div>
             <div className="box box2">
-              <span>
-                Sejak tahun 2015 dipercaya oleh ribuan pengusaha kuliner di seluruh Indonesia. <FontAwesomeIcon icon={faQuoteLeftAlt} className='icon' />
+              <span>10 Tahun<span>
+
+                Indopowder telah menjadi pilihan utama pengusaha kuliner berkat kualitas dan kepercayaan yang telah dibangun sejak 2015 <FontAwesomeIcon icon={faQuoteLeftAlt} className='icon' />
+              </span>
               </span>
             </div>
             <div className="box box3">
               <FontAwesomeIcon icon={faGlassCheers} className='icon' />
               <span>
-                Kami menyediakan juga sample rasa jika Anda ragu
+                Tidak yakin? Coba dulu produk kami dengan sampel rasa gratis sebelum Anda membeli dalam jumlah besar.
               </span>
             </div>
             <div className="box box4">
-              <span>
-                Bahan baku bersertifikasi <span>Halal</span> dari MUI, PIRT dan Dinas Kesehatan.
-              </span>
+              <span>Halal<span>Kami memastikan setiap produk memenuhi standar kualitas dan kehalalan untuk kepuasan dan keamanan konsumen.</span></span>
             </div>
             <div className="box box5">
-              <FontAwesomeIcon icon={faMoneyBill} className='icon' />
-              <span>
-                Ribuan pembeli, agent Dan reseller telah membeli, menggunakan dan menjual kembali produk kami.
-              </span>
+              <FontAwesomeIcon icon={faUserFriends} className='icon' />
+              <span>Jadilah bagian dari komunitas besar yang telah merasakan manfaat dari produk kami yang berkualitas.</span>
             </div>
             <div className="box box6">
-              <span>
-                1Kg Untuk 25 Gelass! <span>kebayangkan untungnya berapa?</span>
-              </span>
+              <span>Ekonomis <span>Produk kami sangat ekonomis dan efisien, membuat bisnis minuman Anda lebih menguntungkan.</span></span>
             </div>
           </div>
         </div>
@@ -56,7 +52,7 @@ const AboutPage = () => {
             <div className="box box2">
               <h1 className="title">Rajanya Bubuk Minuman</h1>
               <p className="details">
-                Sejak Tahun 2015 Dipercaya Oleh Ribuan Pengusaha Kuliner Di Seluruh Indonesia Mulai Dari Pengusaha UMKM Hingga Hotel,Cafe & Resto. Dengan Proses Produksi Bubuk Minuman Yang Higienis, Bahan Baku Yang Bersertifikasi Halal Dari MUI Dan PIRT Dari Dinas Kesehatan, Indopowder Menjadi Pilihan Tepat Bagi Seluruh Pelaku Usaha Kuliner Untuk Menjadi Supplier Bubuk Minuman.
+                Sejak tahun 2015 dipercaya oleh ribuan pengusaha kuliner di seluruh Indonesia mulai dari pengusaha UMKM hingga hotel, cafe & resto. Dengan proses produksi bubuk minuman yang higienis, bahan baku yang bersertifikasi halal dari MUI dan PIRT dari Dinas Kesehatan, Indopowder menjadi pilihan tepat bagi seluruh pelaku usaha kuliner untuk menjadi supplier bubuk minuman.
               </p>
             </div>
           </div>
@@ -65,19 +61,39 @@ const AboutPage = () => {
         <div className="row3">
           <div className="gridRow3">
             <div className="box box1">
-              <h1 className="title">Nikmati Kesegaran Alami</h1>
-              <div className="boxImage">
-                <Image src="/assets/images/boba-vector-2024-v1.png" width={300} height={300} alt='indopowder tangsel' />
+              <div className="imageWrapper">
+                <Image src="/assets/images/about-indopowdertangsel-rev-1.jpg" layout="fill" alt='indopowder tangsel' />
               </div>
-
             </div>
-            <div className="box box2">Box2</div>
-            <div className="box box3">Box3</div>
-            <div className="box box4">Mari Kita Bersama-Sama Wujudkan Usaha Impian Anda.</div>
-            <div className="box box5">Box5</div>
-            <div className="box box6">Box6</div>
-            <div className="box box7">Box7</div>
-            <div className="box box8">Urusan Bubuk Minuman Percayakan Pada Kami Ahlinya</div>
+            <div className="box box2">
+              <div className="imageWrapper">
+                <Image src="/assets/images/about-indopowdertangsel-rev-2.jpg" layout='fill' alt='indopowder tangsel' />
+              </div>
+            </div>
+            <div className="box box3">
+              <div className="imageWrapper">
+                <Image src="/assets/images/about-indopowdertangsel-rev-5.jpg" layout='fill' alt='indopowder tangsel' />
+              </div>
+            </div>
+            <div className="box box4">Mari Kita Bersama-Sama Wujudkan Usaha Impian Anda. Indopowder telah menjadi pilihan utama pengusaha kuliner berkat kualitas dan kepercayaan yang telah dibangun sejak 2015</div>
+            <div className="box box5">
+              <div className="imageWrapper">
+                <Image src="/assets/images/about-indopowdertangsel-rev-4.jpg" layout='fill' alt='indopowder tangsel' />
+              </div>
+            </div>
+            <div className="box box6">
+              <div className="imageWrapper">
+                <Image src="/assets/images/about-indopowdertangsel-rev-3.jpg" layout='fill' alt='indopowder tangsel' />
+              </div>
+            </div>
+            <div className="box box7">
+              <div className="imageWrapper">
+                <Image src="/assets/images/about-indopowdertangsel-rev-6.jpg" layout='fill' alt='indopowder tangsel' />
+              </div>
+            </div>
+            <div className="box box8">
+              Indopowder telah menjadi pilihan utama pengusaha kuliner berkat kualitas dan kepercayaan yang telah dibangun sejak 2015
+            </div>
           </div>
         </div>
       </div>

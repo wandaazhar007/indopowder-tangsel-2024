e11a779aea9a9cdcef58ad2ebe05841cce714dfc
wanda azhar <wandaazhar@gmail.com>
2024-11-03 01:53:14 -0400

    adjustment position the new navbar

--- app/components/navbarNew/NavbarNew.tsx
+++ app/components/navbarNew/NavbarNew.tsx
@@ -2,26 +2,27 @@ import React from "react";
 import "./navbarNew.scss"; // Ensure this file exists for styling
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <header className="navbar">
       <div className="navbarContainer">
-        <div className="navbarLogo">
-          <a href="/" aria-label="TechCrunch Home">
-            <h1>Indopowder</h1>
-          </a>
+        <div className="navbarLeft">
+          <div className="navbarLogo">
+            <Link href="/" aria-label="TechCrunch Home"><h1>Indopowder</h1></Link>
+          </div>
+          <nav className="navbarLinks">
+            <ul>
+              <li><Link href="/about">About</Link></li>
+              <li><Link href="/products">Product</Link></li>
+              <li><Link href="/faq">Faq &apos; s</Link></li>
+              <li><Link href="/contact">Contact</Link></li>
+            </ul>
+          </nav>
         </div>
-        <nav className="navbarLinks">
-          <ul>
-            <li><a href="#latest">About</a></li>
-            <li><a href="#startups">Product</a></li>
-            <li><a href="#venture">Faq &apos; s</a></li>
-            <li><a href="#apple">Contact</a></li>
-          </ul>
-        </nav>
         <div className="navbarActions">
-          <a href="#signin" className="navbarSignin" aria-label="Sign In">Sign In</a>
+          <Link href="#signin" className="navbarSignin" aria-label="Sign In">Sign In</Link>
           <button className="navbarSearch" aria-label="Search">
             {/* <i className="fas fa-search"></i> */}
             <FontAwesomeIcon icon={faSearch} />
